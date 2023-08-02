ac0071cea05e3813cf287bc6e3ebb1d0baf29bfa
jaseel-koolath <112375897+jaseel-koolath@users.noreply.github.com>
2023-08-02 21:17:15 +0530

    complete exercise

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const { URL } = require("url");
+const dns = require("dns");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,15 +11,40 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
-
+app.use(bodyParser.urlencoded({ extended: false }));
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
+});
+const shortenedURLS = {};
+app.post("/api/shorturl", (req, res) => {
+  const originalURL = req.body.url;
+  const urlObject = new URL(originalURL);
+  dns.lookup(urlObject.hostname, (err, address, family) => {
+    if (err) {
+      res.json({
+        originalURL: originalURL,
+        shortenedURL: "Invalid URL",
+      });
+    } else {
+      var shortenedURL = Math.floor(Math.random() * 100000).toString();
+      shortenedURLS[shortenedURL] = originalURL;
+      res.json({
+        originalURL: originalURL,
+        shortenedURL: shortenedURL,
+      });
+    }
+  });
+});
+app.get("/api/shorturl/:url", (req, res) => {
+  console.log(req.params, shortenedURLS[req.params.url]);
+  const to = shortenedURLS[req.params.url];
+  res.redirect(to);
 });
 
 app.listen(port, function() {
